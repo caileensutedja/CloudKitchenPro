61de7714e12de165bf0a425c03a80498b922ad65
caileenstj <csut0022@student.monash.edu>
2025-10-18 14:34:24 +1100

    Created the dynamic headers based on roles and footer

--- A3app/src/app/header/header.ts
+++ A3app/src/app/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth-service.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.css'
 })
 export class Header {
+  // role : string = '';
+  constructor(public auth: AuthService, private router: Router) {}
+
+    logout() {
+      this.auth.logout();
+      this.router.navigate(['/34375783/user/login']);
+    }
+
+    isLoggedIn() {
+      return this.auth.isLoggedIn();
+    }
+
+    get role(): 'chef' | 'admin' | 'manager' | null {
+    return this.auth.role;
+    }
 
 }
